1bbe2d356d1ac15750e74a373353de03778d6482
BalaChi <73782089+BalaChi@users.noreply.github.com>
2021-08-21 13:01:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var a = "Click the refresh buttton again and again to play the game!";
-alert(a);
+
 var random_number = Math.floor(Math.random()*6)+1;
 var random_choice = "images/dice"+random_number+".png";
 var changing_attribute = document.querySelectorAll("img")[0];
